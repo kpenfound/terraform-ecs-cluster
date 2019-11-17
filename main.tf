9ceedc10b1277a26de41b2eb78d7916471b9632a
kpenfound <kpenfound11@gmail.com>
2019-11-17 16:41:53 -0500

    attach policy

--- main.tf
+++ main.tf
@@ -29,27 +29,51 @@ resource "aws_iam_role" "ecs_instance_role" {
         "Service": "ec2.amazonaws.com"
       },
       "Action": [
-        "sts:AssumeRole",
-        "ec2:DescribeTags",
-        "ecs:CreateCluster",
-        "ecs:DeregisterContainerInstance",
-        "ecs:DiscoverPollEndpoint",
-        "ecs:Poll",
-        "ecs:RegisterContainerInstance",
-        "ecs:StartTelemetrySession",
-        "ecs:UpdateContainerInstancesState",
-        "ecs:Submit*",
-        "ecr:GetAuthorizationToken",
-        "ecr:BatchCheckLayerAvailability",
-        "ecr:GetDownloadUrlForLayer",
-        "ecr:BatchGetImage",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
+        "sts:AssumeRole"
       ]
     }
   ]
 }
 EOF
+}
+
+resource "aws_iam_policy" "instance_policy" {
+  name        = "ecs-instance-polciy"
+  description = "ECS Instance Policy"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeTags",
+                "ecs:CreateCluster",
+                "ecs:DeregisterContainerInstance",
+                "ecs:DiscoverPollEndpoint",
+                "ecs:Poll",
+                "ecs:RegisterContainerInstance",
+                "ecs:StartTelemetrySession",
+                "ecs:UpdateContainerInstancesState",
+                "ecs:Submit*",
+                "ecr:GetAuthorizationToken",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "instance_policy_attach" {
+  role       = "${aws_iam_role.ecs_instance_role.name}"
+  policy_arn = "${aws_iam_policy.instance_policy.arn}"
 }
 
 resource "aws_launch_configuration" "ecs_instance" {
