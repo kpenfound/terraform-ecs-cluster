58ae63a877f0887c4b3106b781674a3803604fbf
kpenfound <kpenfound11@gmail.com>
2019-11-17 17:04:01 -0500

    Example worked out

--- example/main.tf
+++ example/main.tf
@@ -11,24 +11,20 @@ resource "aws_default_vpc" "default" {
 
 data "aws_subnet_ids" "default" {
   vpc_id = "${aws_default_vpc.default.id}"
-
-  tags = {
-    Tier = "Private"
-  }
 }
 
 resource "aws_key_pair" "cluster_key" {
-  key_name = "ecs-cluster"
+  key_name   = "ecs-cluster"
   public_key = "${file(var.public_key_file)}"
 }
 
 module "ecs_cluster" {
-  source = "github.com/kpenfound/ecs-cluster"
+  source = "github.com/kpenfound/ecs-cluster?ref=1.0.0"
 
-  region = "${var.region}"
-  ecs_ami = "${var.ecs_ami}"
+  region           = "${var.region}"
+  ecs_ami          = "${var.ecs_ami}"
   ecs_instance_key = "${aws_key_pair.cluster_key.key_name}"
-  cluster_name = "ecs_cluster_example"
-  vpc_id = "${aws_default_vpc.default.id}"
-  subnets = "${data.aws_subnet_ids.default.ids}"
+  cluster_name     = "ecs_cluster_example"
+  vpc_id           = "${aws_default_vpc.default.id}"
+  subnets          = "${data.aws_subnet_ids.default.ids}"
 }
