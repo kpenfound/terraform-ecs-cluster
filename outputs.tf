eed9fcfe8e914002e492a2661677ed8dac6010ae
kpenfound <kpenfound11@gmail.com>
2019-11-20 23:15:01 -0500

    fmt

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "ecs_asg" {
-  value = aws_autoscaling_group.ecs
+  value       = aws_autoscaling_group.ecs
   description = "The AWS Autoscaling group associated with the ECS cluster"
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable region {
-  default = "us-east-1"
+  default     = "us-east-1"
   description = "The AWS region to deploy to"
 }
 variable ecs_ami {
-  default = "ami-08fa2eb77f5afe360"
+  default     = "ami-08fa2eb77f5afe360"
   description = "The ECS AMI to use for the ECS host. Current AMIs found here: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html"
 }
 
 variable ecs_instance_type {
-  default = "t2.micro"
+  default     = "t2.micro"
   description = "EC2 instance type for the ECS hosts"
 }
 variable ecs_instance_key {
@@ -16,16 +16,16 @@ variable ecs_instance_key {
 }
 
 variable cluster_name {
-  default = "default"
+  default     = "default"
   description = "The ECS cluster name"
 }
 
 variable cluster_min_size {
-  default = 1
+  default     = 1
   description = "Minimum available ECS hosts configured in the auto-scaling group"
 }
 variable cluster_max_size {
-  default = 1
+  default     = 1
   description = "Maximum available ECS hosts configured in the auto-scaling group"
 }
 
