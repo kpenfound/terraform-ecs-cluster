0d4b8f0e75b93d7bb00827995007cdb9613dd0c9
Kyle Penfound <kpenfound11@gmail.com>
2020-12-16 16:09:03 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ecs_asg" {
   value       = aws_autoscaling_group.ecs
   description = "The AWS Autoscaling group associated with the ECS cluster"
 }
+
+output "sg_id" {
+  value       = aws_security_group.cluster_instance.id
+  description = "The id of the clusters security group"
+}
